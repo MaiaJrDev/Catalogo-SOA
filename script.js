dae7a389a488d2a28703aafdeb768b55284393b9
MaiaJrDev <rm.dcjr@hotmail.com>
2021-08-20 23:34:30 -0300

    atualização

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 var jaxT = document.getElementById('jax');
 var clayM = document.getElementById('clay');
-var juice = document.getElementById('juice');
-var chibs = document.getElementById('chibs');
-var desc1 = document.getElementById('desc1');
-var desc2 = document.getElementById('desc2');
-var desc3 = document.getElementById('desc3');
-var desc4 = document.getElementById('desc4');
-var btn = document.getElementById('btnInf');
-var btn2 = document.getElementById('btnInf2');
-var btn3 = document.getElementById('btnInf3');
-var btn4 = document.getElementById('btnInf4');
+
+var btn = [];
+//cria um array com as informações dos botões
+for(var a = 0; a <= 8; a++){
+   btn[a] = document.getElementById('btnInf' +a);
+
+}
 
 
 function infoJax(){
-   
+    //oculta todos os outros personagens
     clayM.setAttribute('style', 'display:none');
     chibs.setAttribute('style', 'display:none');
     juice.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display: none');
+    happy.setAttribute('style', 'display: none');
+    bob.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
+    //exibe a descrição
     desc1.setAttribute('style', 'display: block');
-    btn.setAttribute('style', 'display:none');
+    //oculta o botão de informações
+    btn[1].setAttribute('style', 'display:none');
+    //cria o botão para voltar
     let criaBtn = document.createElement('button');
     let boron = document.getElementById('btnVoltar1').appendChild(criaBtn);
+    //atribui a classe do bootstrap ao botão
     boron.setAttribute('class', 'btn btn-info');
+    //define a função do botão
     boron.setAttribute('onclick', 'window.location.reload();')
     boron.innerText = "VOLTAR";
 
@@ -32,8 +38,12 @@ function infoClay(){
     jaxT.setAttribute('style', 'display:none');
     juice.setAttribute('style', 'display:none');
     chibs.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display: none');
+    happy.setAttribute('style', 'display: none');
+    bob.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
     desc2.setAttribute('style', 'display: block');
-    btn2.setAttribute('style', 'display:none');
+    btn[2].setAttribute('style', 'display:none');
     let criaBtn = document.createElement('button');
     let boron = document.getElementById('btnVoltar2').appendChild(criaBtn);
     boron.setAttribute('class', 'btn btn-info');
@@ -47,8 +57,12 @@ function infoJuice(){
     jaxT.setAttribute('style', 'display:none');
     chibs.setAttribute('style', 'display:none');
     clayM.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display: none');
+    happy.setAttribute('style', 'display: none');
+    bob.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
     desc3.setAttribute('style', 'display: block');
-    btn3.setAttribute('style', 'display:none');
+    btn[3].setAttribute('style', 'display:none');
     let criaBtn = document.createElement('button');
     let boron = document.getElementById('btnVoltar3').appendChild(criaBtn);
     boron.setAttribute('class', 'btn btn-info');
@@ -62,10 +76,90 @@ function infoChibs(){
     jaxT.setAttribute('style', 'display:none');
     clayM.setAttribute('style', 'display:none');
     juice.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display: none');
+    happy.setAttribute('style', 'display: none');
+    bob.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
     desc4.setAttribute('style', 'display: block');
-    btn4.setAttribute('style', 'display:none');
+    btn[4].setAttribute('style', 'display:none');
     let criaBtn = document.createElement('button');
     let boron = document.getElementById('btnVoltar4').appendChild(criaBtn);
+    boron.setAttribute('class', 'btn btn-info');
+    boron.setAttribute('onclick', 'window.location.reload();')
+    boron.innerText = "VOLTAR";
+    
+
+}
+function infoTig(){
+   
+    jaxT.setAttribute('style', 'display:none');
+    clayM.setAttribute('style', 'display:none');
+    juice.setAttribute('style', 'display:none');
+    chibs.setAttribute('style', 'display:none');
+    happy.setAttribute('style', 'display: none');
+    bob.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
+    desc5.setAttribute('style', 'display: block');
+    btn[5].setAttribute('style', 'display:none');
+    let criaBtn = document.createElement('button');
+    let boron = document.getElementById('btnVoltar5').appendChild(criaBtn);
+    boron.setAttribute('class', 'btn btn-info');
+    boron.setAttribute('onclick', 'window.location.reload();')
+    boron.innerText = "VOLTAR";
+    
+
+}
+function infoHappy(){
+   
+    jaxT.setAttribute('style', 'display:none');
+    clayM.setAttribute('style', 'display:none');
+    juice.setAttribute('style', 'display:none');
+    chibs.setAttribute('style', 'display:none');
+    bob.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
+    desc6.setAttribute('style', 'display: block');
+    btn[6].setAttribute('style', 'display:none');
+    let criaBtn = document.createElement('button');
+    let boron = document.getElementById('btnVoltar6').appendChild(criaBtn);
+    boron.setAttribute('class', 'btn btn-info');
+    boron.setAttribute('onclick', 'window.location.reload();')
+    boron.innerText = "VOLTAR";
+    
+
+}
+function infoBob(){
+   
+    jaxT.setAttribute('style', 'display:none');
+    clayM.setAttribute('style', 'display:none');
+    juice.setAttribute('style', 'display:none');
+    chibs.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display:none');
+    happy.setAttribute('style', 'display:none');
+    opie.setAttribute('style', 'display:none');
+    desc7.setAttribute('style', 'display: block');
+    btn[7].setAttribute('style', 'display:none');
+    let criaBtn = document.createElement('button');
+    let boron = document.getElementById('btnVoltar7').appendChild(criaBtn);
+    boron.setAttribute('class', 'btn btn-info');
+    boron.setAttribute('onclick', 'window.location.reload();')
+    boron.innerText = "VOLTAR";
+    
+
+}
+function infoOpie(){
+   
+    jaxT.setAttribute('style', 'display:none');
+    clayM.setAttribute('style', 'display:none');
+    juice.setAttribute('style', 'display:none');
+    chibs.setAttribute('style', 'display:none');
+    tig.setAttribute('style', 'display:none');
+    bob.setAttribute('style', 'display:none');
+    happy.setAttribute('style', 'display:none');
+    desc8.setAttribute('style', 'display: block');
+    btn[8].setAttribute('style', 'display:none');
+    let criaBtn = document.createElement('button');
+    let boron = document.getElementById('btnVoltar8').appendChild(criaBtn);
     boron.setAttribute('class', 'btn btn-info');
     boron.setAttribute('onclick', 'window.location.reload();')
     boron.innerText = "VOLTAR";
